d60d307d6332eb6e7b63586d9c1e781608f8b2ff
Igor Chapy <misha@mate.acedemy>
2025-04-23 17:56:50 +0300

    Project

---
+++ cinema/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
--- migrations/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from cinema.models import Movie
-
-
-class CinemaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ["id", "title", "description", "duration"]
-        read_only_fields = ["id"]
