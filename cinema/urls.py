3b9f55ea5d13584a98e7d5562355d8f405d861c7
Igor Chapy <misha@mate.acedemy>
2025-04-24 19:56:17 +0300

    fix the problem

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import MovieListCreateAPIView, MovieRetrieveUpdateDestroyAPIView
 
+
 urlpatterns = [
     path('movies/', MovieListCreateAPIView.as_view(), name='movie-list-create'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyAPIView.as_view(), name='movie-detail'),
--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import Movie
 from .serializers import MovieSerializer
 
+
 class MovieListCreateAPIView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
